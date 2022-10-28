58cd0b48842066dc462f63c6323767042d5bd5f7
Ralph Kube <ralph_kube@gmx.net>
2022-10-28 08:09:33 -0400

    Cleaning up

--- examples/test_iteration.py
+++ examples/test_iteration.py
@@ -1,7 +1,6 @@
 #-*- coding: utf-8 -*-
 
 # First attempt at training FRNN-style
-
 import logging
 import shutil
 import errno
@@ -23,27 +22,31 @@ from frnn_loader.loaders.frnn_dataset_disk import shot_dataset_disk
 from frnn_loader.loaders.frnn_multi_dataset import frnn_multi_dataset
 from frnn_loader.loaders.frnn_loader import batched_random_sequence_sampler
 
-
+# This specifies the root path under which all HDF5 data will be stored
 root = "/projects/FRNN/frnn_loader"
-
+# Work on 2 shots 
 shotlist = [180619, 180620]
-signal_list = ["dssdenest", "q95", "echpwrc", "pradcore", "ipsiptargt"]
 
 # Instantiate resampler, etc.
 my_resampler = resampler_causal(0.0, 2e3, 1e0)
-# Instantiate a file backend
+# Instantiate a file backend. This object handles file storage
 my_backend_file = backend_hdf5(root)
+# Fetchers handle data fetching from remote targets
 my_fetcher = fetcher_d3d_v1()
-pred_list = [signal_0d(n) for n in signal_list]
+# Specify a list of predictors. See frnn_loader/frnn_loader/data/d3d_signals.yaml for definitions
+pred_list = [signal_0d(n) for n in ["dssdenest", "q95", "echpwrc", "pradcore", "ipsiptargt"]a]
 
+# Generate a list of datasets, one for each shot.
+# These datasets cache all data in a hdf5-file on disk.
 ds_list = [shot_dataset_disk(shotnr,
                              predictors=pred_list,
                              resampler=my_resampler,
                              backend_file=my_backend_file,
                              fetcher=my_fetcher,
-                             root = root,
+                             root=root,
                              download=True,
                              dtype=torch.float32) for shotnr in shotlist]
+# Generate a multi-shot dataset from the list of data-sets(similar to shotlist)
 ds_multi = frnn_multi_dataset(ds_list)
 
 
@@ -60,10 +63,15 @@ def my_collate_fn(input):
 
     return X_vals, Y_vals
 
+# This sampler pulls a batch of `batch_size` sequences, each of length `seq_length
+# from the dataset. The sequences start at random places and are uniformly distributed
+# over the shots of the dataset.
 my_sampler = batched_random_sequence_sampler(ds_list, seq_length=100, batch_size=4)
+# Default pytorch dataloader. Batch shape is defined through my_sampler
+# my_collate_fn is used to transform output from dataset.__getitem__ to a torch.tensor
 my_loader = DataLoader(ds_multi, sampler=my_sampler, collate_fn=my_collate_fn)
 
-# test no-batching
+# Iterate over training set
 num_batches = 0
 for v in my_loader:
     print(v[0].shape, v[1].shape)
